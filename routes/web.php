099fb854f098fd9a9c2b75110a99a8511c16cdda
Rakesh <rakesh.bokde@arsenaltech.com>
2023-03-29 13:45:30 +0530

    Working on progress

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\AuthorController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('author')->name('author')->group(function(){
+
+    Route::middleware(['guest:web'])->group(function(){
+        Route::view('/login', 'back.pages.auth.login')->name('login');
+        Route::view('/forgot-password', 'back.pages.auth.forgot')->name('forgot-password');
+    });
+    Route::middleware([])->group(function(){
+        Route::get('/home', [AuthorController::class, 'index'])->name('home');
+        Route::view('/forgot-password', 'back.pages.auth.forgot')->name('forgot-password');
+    });
 });
